c5c505cd7ac91665950126a602de25ed72c68ef4
Lathusan2002 <lathusanlathusan40@gmail.com>
2025-03-04 11:26:13 +0530

    add program

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Program from './Components/Program/Program'
+import Title from './Components/Title/Title'
+
+
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+    <div className="container">
+    <Title subTitle="Our PROGRAM" title="What We Offer" />
+      <Program/>
+      </div>
     </div>
   )
 }
