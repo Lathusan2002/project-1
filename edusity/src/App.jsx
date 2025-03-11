2e7391e4e264f1c700a82aac68069767dbb63ca7
Lathusan2002 <lathusanlathusan40@gmail.com>
2025-03-11 09:44:36 +0530

    add

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -7,6 +7,7 @@ import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 
 
@@ -25,6 +26,7 @@ const App = () => {
       <Testimonials/>
       <Title subTitle="Contact us" title="Get in Touch" />
       <Contact/>
+      <Footer/>
       
       </div>
     </div>
